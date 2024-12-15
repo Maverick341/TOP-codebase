f2d4869956a6e587e09df12fc8bc3c8adf0fd4d2
Maverick341 <shinde341sushant@gmail.com>
2024-12-15 21:29:51 +0530

    object prototypes done

--- javascript exercises/Objects and Objects Constructors/Code-along1/script.js
+++ javascript exercises/Objects and Objects Constructors/Code-along1/script.js
@@ -1,12 +1,46 @@
-function Player(name, marker) {
+// function Player(name, marker) {
+//     this.name = name;
+//     this.marker = marker;
+//     this.sayName = function () {
+//         console.log(this.name)
+//     };
+// }
+
+// const player1 = new Player('steve', 'X');
+// const player2 = new Player('also steve', 'O');
+// player1.sayName(); // logs 'steve'
+// player2.sayName(); // logs 'also steve'
+
+
+function Person(name) {
+    this.name = name;
+  }
+  
+  Person.prototype.sayName = function() {
+    console.log(`Hello, I'm ${this.name}!`);
+  };
+  
+  function Player(name, marker) {
     this.name = name;
     this.marker = marker;
-    this.sayName = function () {
-        console.log(this.name)
-    };
-}
-
-const player1 = new Player('steve', 'X');
-const player2 = new Player('also steve', 'O');
-player1.sayName(); // logs 'steve'
-player2.sayName(); // logs 'also steve'
+  }
+  
+  Player.prototype.getMarker = function() {
+    console.log(`My marker is '${this.marker}'`);
+  };
+  
+  Object.getPrototypeOf(Player.prototype); // returns Object.prototype
+  
+  // Now make `Player` objects inherit from `Person`
+  Object.setPrototypeOf(Player.prototype, Person.prototype);
+  Object.getPrototypeOf(Player.prototype); // returns Person.prototype
+  
+  const player1 = new Player('steve', 'X');
+  const player2 = new Player('also steve', 'O');
+  
+  player1.sayName(); // Hello, I'm steve!
+  player2.sayName(); // Hello, I'm also steve!
+  
+  player1.getMarker(); // My marker is 'X'
+  player2.getMarker(); // My marker is 'O'
+  
